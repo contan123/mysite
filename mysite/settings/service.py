48e53c2fe958210bf87a03aa1f98d1acf74f87ff
contan123 <3143790685@qq.com>
2021-02-28 19:49:31 +0800

    添加static root 收集动态数据

--- mysite/settings/service.py
+++ mysite/settings/service.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-from pathlib import Path
 from .base import *
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
